6b5879de107eb07ff309a9a216b7af04fe6a3e69
Asraful Islam <asraful009@gmail.com>
2021-04-07 22:04:55 +0600

    play ground

--- DX-BALL-009/Assets/scripts/Ball.cs
+++ DX-BALL-009/Assets/scripts/Ball.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    float _speed = 40f;
+    Vector3 _velocity;
+    Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        Debug.Log(Vector3.down);
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.velocity = Vector3.down * _speed;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
+        _velocity = _rigidbody.velocity;
+
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        _rigidbody.velocity = Vector3.Reflect(_velocity, collision.contacts[0].normal);
     }
 }
